48eb55c2c14dc8d0db5287dc0d6239d9d1756e18
isaias-trujillo <119267321+isaias-trujillo@users.noreply.github.com>
2024-11-16 14:49:06 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMembers() {
+    return [
+        { name: "Joe", skills: ["JavaScript", "React"] },
+        { name: "Sam", skills: ["JavaScript", "Angular"] },
+        { name: "Jack", skills: ["HTML", "CSS"] },
+        { name: "Sarah", skills: ["HTML", "CSS"] },
+        { name: "Jane", skills: ["JavaScript", "NodeJS"] },
+        { name: "Jill", skills: ["Java", "Spring"] },
+        { name: "David", skills: ["Java", "Spring"] },
+        { name: "Eli", skills: ["Java", "Spring"] },
+    ];
+}
